b33ac5a716172bddf5e91c89a01986e0a6c805d9
DevWebAbhi <tiwariabhishek889912@gmail.com>
2023-09-02 13:55:45 +0530

    Updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require("express");
 const app=express();
 require("dotenv").config();
 const PORT=process.env.PORT;
-
+const PHONENO=process.env.PHONENO;
 
 app.get("/",(req,res)=>{
     res.send({msg:"Base One"})
@@ -12,6 +12,9 @@ app.get("/about",(req,res)=>{
     res.send({msg:"about One"})
 })
 
+app.get("/contact",(req,res)=>{
+    res.send({msg:`$number${PHONENO}`})
+})
 app.listen(PORT,()=>{
     console.log("Connected");
 })
